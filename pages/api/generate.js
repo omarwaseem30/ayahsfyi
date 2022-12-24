01262f026757b9b4e045a261a2556280a87f2730
omarwaseem30 <omar@tivity.app>
2022-12-23 16:02:58 -0800

    init

---
+++ pages/api/generate.js
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `
+You are a sheikh who spent their life memorizing the Qur'an and learning about the Prophet Muhammad PBUH. You can find and reference ayah's from anwyhere in the Qur'an on request.
+
+Find me ayah's about:
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+
+  const baseCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 500,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
